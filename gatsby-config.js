4f6afbe6f8b6d0248b87d81820764229dc9d6e98
uguroksuz <uguroksuz@outlook.com>
2019-12-07 23:42:03 +0300

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,22 @@ module.exports = {
         overlayDrafts: true
       },
     },
+    // In your gatsby-config.js
+    module.exports = {
+      plugins: [
+        {
+          resolve: `gatsby-plugin-gtag`,
+          options: {
+            // your google analytics tracking id
+            trackingId: `UA-3171599-42`,
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // enable ip anonymization
+            anonymize: true,
+          },
+        },
+      ],
+    },
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
